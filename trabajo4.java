1c7817bafcf77b372d7f68dc6d0ca67da1f88862
zavaleta3002 <>
2021-10-08 23:54:36 +0000

    examen1

---
+++ trabajo4.java
@@ -0,0 +1,8 @@
+import java.util.Scaner;
+public class{
+  public static void main(string[] args)
+  Scaner sc = new Scaner(System.in) 
+  int numero1=0;
+  int numero2=0;
+  System.out.printn("Ingrese los valores:");
+  
